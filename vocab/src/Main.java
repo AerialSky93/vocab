1ee0e03aac0e113a0b6082e1ae772c3acbfdd0e2
AerialSky93 <rdutta23@yahoo.com>
2023-08-01 00:49:13 -0700

    update comma type

--- vocab/src/Main.java
+++ vocab/src/Main.java
@@ -5,18 +5,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String verbData = "사다,아프다,피곤하다,결혼하다,빨래하다,요리하다,청소하다,졸업하다";
+        String verbData = "조용하다,시끄럽다,다르다,출발하다,들다,지각하다,밝다";
         VerbHelper verbHelper = new VerbHelper();
         List<String> verbList = Arrays.asList(verbData.split("\\s*,\\s*"));
+        String commaType = ",\n";
         for (String verbItem : verbList) {
             String htmlContent = verbHelper.getURLString(verbItem);
-            String test1 = verbHelper.getVerbValue(htmlContent, "Declarative Present", "informal high") + ",";
-            String test2 = verbHelper.getVerbValue(htmlContent, "Declarative Present", "informal low") + ",";
-            String test3 = verbHelper.getVerbValue(htmlContent, "Declarative Past", "informal high") + ",";
-            String test4 = verbHelper.getVerbValue(htmlContent, "Declarative Past", "informal low") + ",";
+            String test1 = verbHelper.getVerbValue(htmlContent, "Declarative Present", "informal high") + commaType;
+            String test2 = verbHelper.getVerbValue(htmlContent, "Declarative Present", "informal low") + commaType;
+            String test3 = verbHelper.getVerbValue(htmlContent, "Declarative Past", "informal high") + commaType;
+            String test4 = verbHelper.getVerbValue(htmlContent, "Declarative Past", "informal low") + commaType;
             String test5 = verbHelper.getVerbValue(htmlContent, "Declarative Future", "informal high") + ",";
-            String test6 = verbHelper.getVerbValue(htmlContent, "Declarative Present", " formal high") + ",";
-            System.out.println(verbItem + ", \n" + test1 + "\n" + test2 + "\n" + test3+ "\n" + test4+ "\n" + test5+ "\n" + test6);
+            //String test6 = verbHelper.getVerbValue(htmlContent, "Declarative Present", " formal high") + ",";
+            System.out.println(verbItem + ",\n" + test1 + test2 + test3 + test4 + test5);
         }
 
     }
